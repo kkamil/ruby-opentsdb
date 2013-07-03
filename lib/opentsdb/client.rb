c9694cd17b8b762f634609c876bfddb146a5e620
Kamil <kamil.kuzma@cloudvertical.com>
2013-07-03 07:26:52 +0200

    add query

--- lib/opentsdb/client.rb
+++ lib/opentsdb/client.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'opentsdb/logging'
+require 'net/http'
 
 module OpenTSDB
   class Client
@@ -9,9 +10,9 @@ module OpenTSDB
 
     def initialize(options = {})
       begin
-        hostname = options[:hostname] || 'localhost'
-        port = options[:port] || 4242
-        @connection = TCPSocket.new(hostname, port)
+        @hostname = options[:hostname] || 'localhost'
+        @port = options[:port] || 4242
+        @connection = TCPSocket.new(@hostname, @port)
       rescue
         raise "Unable to connect or invalid connection data"
       end
@@ -23,6 +24,15 @@ module OpenTSDB
       value = options[:value].to_f
       tags = options[:tags].map{|k,v| "#{k}=#{v}"}.join(" ")
       @connection.puts("put #{metric_name} #{timestamp} #{value} #{tags}")
+    end
+
+    def query(options = {})
+      start_date = options[:start_date].strftime("%Y/%m/%d-%H:%M:%S")
+      end_date = options[:end_date].strftime("%Y/%m/%d-%H:%M:%S")
+      aggr = options[:aggr]
+      metric = options[:metric]
+      query = "/q?start=#{start_date}&end=#{end_date}&m=#{aggr}:#{metric}&ascii"
+      Net::HTTP.get(@hostname, query, @port).scan(/\w+ ([0-9]+) ([-+]?[0-9]+\.?[0-9]*)/).map{|el| OpenStruct.new(:created_at => el[0].to_i, :value => el[1].to_f)}
     end
   end
 end
